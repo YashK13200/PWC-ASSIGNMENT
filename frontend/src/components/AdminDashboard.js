f153f4c581f0ef0f7801d3a1873947e8c4d1ab56
Yash Kr Kashyap <111449195+YashK13200@users.noreply.github.com>
2024-10-22 20:30:04 +0530

    Update AdminDashboard.js

--- frontend/src/components/AdminDashboard.js
+++ frontend/src/components/AdminDashboard.js
@@ -1,39 +1,3 @@
-// // frontend/src/components/AdminDashboard.js
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-
-// const AdminDashboard = () => {
-//     const [users, setUsers] = useState([]);
-
-//     useEffect(() => {
-//         const fetchUsers = async () => {
-//             const res = await axios.get('http://localhost:5000/api/users');
-//             setUsers(res.data);
-//         };
-
-//         fetchUsers();
-//     }, []);
-
-//     return (
-//         <div className="admin-dashboard">
-//             <h2>Admin Dashboard</h2>
-//             {users.map(user => (
-//                 <div key={user._id} className="user-card">
-//                     <h3>{user.name}</h3>
-//                     <p>{user.socialMediaHandle}</p>
-//                     {user.images.map(image => (
-//                         <img key={image} src={`http://localhost:5000/${image}`} alt="uploaded" />
-//                     ))}
-//                 </div>
-//             ))}
-//         </div>
-//     );
-// };
-
-// export default AdminDashboard;
-
-// new code here
-
 // frontend/src/components/AdminDashboard.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
@@ -44,7 +8,7 @@ const AdminDashboard = ({ submissionCount }) => {  // Accept 'submissionCount' p
     useEffect(() => {
         const fetchUsers = async () => {
             try {
-                const res = await axios.get('http://localhost:5000/api/users');
+                const res = await axios.get('https://pwc-assignment-a69n.vercel.app/api/users');
                 setUsers(res.data);
             } catch (err) {
                 console.error(err);
@@ -62,7 +26,7 @@ const AdminDashboard = ({ submissionCount }) => {  // Accept 'submissionCount' p
                     <h3>{user.name}</h3>
                     <p>{user.socialMediaHandle}</p>
                     {user.images.map(image => (
-                        <img key={image} src={`http://localhost:5000/${image}`} alt="uploaded" />
+                        <img key={image} src={`https://pwc-assignment-a69n.vercel.app/${image}`} alt="uploaded" />
                     ))}
                 </div>
             ))}
